2b4efe0c0a337cf2fef06680ddd7dd0b444c221f
Clara Hoffmann <claracharlotteh@web.de>
2025-03-30 18:37:59 +0200

    move entropy grad computation to separate function

--- uivi/train.py
+++ uivi/train.py
@@ -6,6 +6,33 @@ from torch import optim
 from torch.distributions import MultivariateNormal
 
 
+def get_ent_grad(model, epsilon, z_sample, num_hmc_samples, mu, sigma):
+    with torch.no_grad():
+        # run hmc for reverse conditional
+        eps_prime, accept_prob = model.run_hmc(epsilon, z_sample)
+        mu_prime, _,_, sigma_sample_prime = model.forward(eps_prime.reshape(-1, model.dim_eps))
+
+        # reshape to account for hmc sample dim
+        mu_prime = mu_prime.reshape(num_hmc_samples, model.num_eps_samples, model.dim_z)
+        sigma_sample_prime = sigma_sample_prime.reshape(num_hmc_samples, model.num_eps_samples, model.dim_z)
+        z_sample_prime = z_sample.unsqueeze(0).repeat(num_hmc_samples, 1, 1)
+
+        # compute entropy gradient w.r.t. z
+        grad_z = - ((z_sample_prime - mu_prime)/(sigma_sample_prime**2))
+
+        # first mc integrate out epsilon, then average over
+        # hmc samples
+        grad_z = grad_z.mean(dim = 0).mean(dim = 0)
+
+        # compute entropy loss for tracking
+        Dz = MultivariateNormal(loc = mu, covariance_matrix = torch.diag_embed(sigma**2))
+        log_qz =  Dz.log_prob(z_sample)
+        log_qz = log_qz.mean()
+    
+    return grad_z, accept_prob, log_qz
+
+
+
 def train_uivi(model, lr = 1e-3,  num_iter = 5000, num_hmc_samples = 5 ):
     """Train UI-VI model."""
     model.train()
@@ -26,27 +53,7 @@ def train_uivi(model, lr = 1e-3,  num_iter = 5000, num_hmc_samples = 5 ):
             loss = -  model.log_p_z(z_sample).mean()
             loss.backward(retain_graph = True) 
 
-            with torch.no_grad():
-                model_hmc = model
-                eps_prime, accept_prob = model_hmc.run_hmc(epsilon, z_sample)
-                mu_prime, _,_, sigma_sample_prime = model_hmc.forward(eps_prime.reshape(-1, model_hmc.dim_eps))
-                
-                mu_prime = mu_prime.reshape(num_hmc_samples, model_hmc.num_eps_samples, model.dim_z)
-                sigma_sample_prime = sigma_sample_prime.reshape(num_hmc_samples, model_hmc.num_eps_samples, model.dim_z)
-                z_sample_prime = z_sample.unsqueeze(0).repeat(num_hmc_samples, 1, 1)
-
-                # compute entropy gradient w.r.t. z
-                grad_z = - ((z_sample_prime - mu_prime)/(sigma_sample_prime**2))
-                
-                # first mc integrate out epsilon, then average over
-                # hmc samples
-                grad_z = grad_z.mean(dim = 0).mean(dim = 0)
-
-                # compute entropy loss for tracking
-                Dz = MultivariateNormal(loc = mu, covariance_matrix = torch.diag_embed(sigma**2))
-                log_qz =  Dz.log_prob(z_sample)
-                log_qz = log_qz.mean()
-            
+            grad_z, accept_prob, log_qz = get_ent_grad(model, epsilon, z_sample, num_hmc_samples, mu, sigma)
             z_sample.grad += grad_z.detach()
             
             # Compute new gradients w.r.t. model parameters using the modified z_sample.grad
@@ -62,9 +69,13 @@ def train_uivi(model, lr = 1e-3,  num_iter = 5000, num_hmc_samples = 5 ):
             losses_mod.append(loss.item())
             losses_ent.append(log_qz.item())
             
-            pbar.set_postfix(loss=loss.item(), log_qz=log_qz.item(), 
-                             grad_z = grad_z.mean().item(), 
-                             accept_prob = accept_prob.mean().item(),  status="running")
+            pbar.set_postfix(
+                loss=f"{loss.item():05.2f}",
+                log_qz=f"{log_qz.item():05.2f}",
+                grad_z=f"{grad_z.mean().item():05.2f}",
+                accept_prob=f"{accept_prob.mean().item():05.2f}",
+                status="running")
+            
             pbar.update(1)
     
     return model, losses_mod, losses_ent
