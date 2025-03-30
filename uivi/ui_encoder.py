e6dbb17591cffdca6afa2db3360768e2bfaba625
Clara Hoffmann <claracharlotteh@web.de>
2025-03-30 19:12:27 +0200

    run hooks

--- uivi/ui_encoder.py
+++ uivi/ui_encoder.py
@@ -1,18 +1,37 @@
 """UIVI encoder class."""
-import torch
-from torch import nn
-from hmc import HMC
-from torch.distributions import MultivariateNormal, Normal
 import lightning as L
+import torch
+from hmc import HMC
+from torch import nn
+from torch.distributions import MultivariateNormal, Normal
 
 
+# pylint: disable = C0116, R0902
 class UIEncoder(L.LightningModule):
-    def __init__(self, mean, cov, num_eps_samples=1, dim_eps = 3, dim_z = 2, T = 5, L = 5, latent_dim = 100):
+    """Encoder with HMC step for the reverse conditional."""
+
+    def __init__(
+        self,
+        mean,
+        cov,
+        num_eps_samples=1,
+        dim_eps=3,
+        dim_z=2,
+        T=5,
+        Ls=5,
+        latent_dim=100,
+    ):
+
         super().__init__()
-        self.encoder_mu_sigma = nn.Sequential(nn.Linear(dim_eps, latent_dim), nn.ReLU(), 
-                                     nn.Linear(latent_dim, latent_dim ), nn.ReLU(), 
-                                     nn.Linear(latent_dim, latent_dim ), nn.ReLU(), 
-                                     nn.Linear(latent_dim, dim_z*2))
+        self.encoder_mu_sigma = nn.Sequential(
+            nn.Linear(dim_eps, latent_dim),
+            nn.ReLU(),
+            nn.Linear(latent_dim, latent_dim),
+            nn.ReLU(),
+            nn.Linear(latent_dim, latent_dim),
+            nn.ReLU(),
+            nn.Linear(latent_dim, dim_z * 2),
+        )
 
         self.num_eps_samples = num_eps_samples
         self.dim_eps = dim_eps
@@ -20,26 +39,29 @@ class UIEncoder(L.LightningModule):
 
         self.mean_batched = mean.repeat(self.num_eps_samples, 1)
         self.cov_batched = cov.repeat(self.num_eps_samples, 1, 1)
-        self.dist = MultivariateNormal(loc = self.mean_batched,
-                                    covariance_matrix = self.cov_batched)
-        
-        self.hmc = HMC(dim_z, T, L)
-        
-    def forward(self, epsilon = None):
+        self.dist = MultivariateNormal(
+            loc=self.mean_batched, covariance_matrix=self.cov_batched
+        )
+
+        self.hmc = HMC(dim_z, T, Ls)
+
+    def forward(self, epsilon=None):
         # 1. Sample input variable
         if epsilon is None:
-            epsilon = torch.randn(self.num_eps_samples, self.dim_eps).requires_grad_(False)
-        
+            epsilon = torch.randn(
+                self.num_eps_samples, self.dim_eps
+            ).requires_grad_(False)
+
         # 2. Forward pass h_\theta(u_s; eps_s)
         out = self.encoder_mu_sigma(epsilon)
-        mu = out[:,:2]
-        sigma = torch.exp(out[:,2:]) + 1e-7
-        u_sample =  torch.randn_like(mu) # sample reparam variable u
-        z_sample = mu + sigma*u_sample
+        mu = out[:, :2]
+        sigma = torch.exp(out[:, 2:]) + 1e-7
+        u_sample = torch.randn_like(mu)  # sample reparam variable u
+        z_sample = mu + sigma * u_sample
         return mu, z_sample, epsilon, sigma
 
     def log_p_z(self, z):
-        coord = torch.stack([z[:, 0], z[:, 1] + z[:, 0] ** 2 + 1 ], dim=1)
+        coord = torch.stack([z[:, 0], z[:, 1] + z[:, 0] ** 2 + 1], dim=1)
         log_p = self.dist.log_prob(coord)
         return log_p
 
@@ -48,7 +70,7 @@ class UIEncoder(L.LightningModule):
             is_train = self.training
             self.eval()
             mu_sample, _, _, sigma_sample = self.forward(epsilon)
-            
+
             if is_train:
                 self.train()
             return self.HMC_bound(epsilon, z_samples, mu_sample, sigma_sample)
@@ -59,10 +81,13 @@ class UIEncoder(L.LightningModule):
         self.register_log_prob(z)
         epsilon_samples, accept_prob = self.hmc(epsilon.detach().clone())
         return epsilon_samples, accept_prob
-    
+
     def HMC_bound(self, epsilon, z_samples, mu_sample, sigma_sample):
-        Dz = MultivariateNormal(loc = mu_sample, covariance_matrix = torch.diag_embed(sigma_sample**2))
-        log_qz_cond_eps_prime =  Dz.log_prob(z_samples)
+        Dz = MultivariateNormal(
+            loc=mu_sample,
+            covariance_matrix=torch.diag_embed(sigma_sample**2),
+        )
+        log_qz_cond_eps_prime = Dz.log_prob(z_samples)
         p_eps_dist = Normal(0, 1)
-        log_p_eps = p_eps_dist.log_prob(epsilon).sum(dim = -1)
+        log_p_eps = p_eps_dist.log_prob(epsilon).sum(dim=-1)
         return log_qz_cond_eps_prime + log_p_eps
